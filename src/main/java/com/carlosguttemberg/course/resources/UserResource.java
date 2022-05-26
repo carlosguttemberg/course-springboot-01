8f780489bf932576ae6835badefd96a04ded4d03
Carlos Guttemberg <guttembergcarlos@gmail.com>
2022-05-25 21:16:06 -0300

    Criando a camada de servico e registro dos componentes

--- src/main/java/com/carlosguttemberg/course/resources/UserResource.java
+++ src/main/java/com/carlosguttemberg/course/resources/UserResource.java
@@ -1,18 +1,31 @@
 package com.carlosguttemberg.course.resources;
 
 import com.carlosguttemberg.course.entities.User;
+import com.carlosguttemberg.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService service;
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Joao", "joao@gmail.com", "(22)99999-9999", "123");
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
 
-        return ResponseEntity.ok().body(user);
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById( @PathVariable Long id) {
+        User obj = service.findById(id);
+
+        return ResponseEntity.ok().body(obj);
     }
 }
