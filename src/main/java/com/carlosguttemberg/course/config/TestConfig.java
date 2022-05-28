bbe1f22950ab2bd2b7ef3ffb62f6fbfebed4133f
Carlos Guttemberg <guttembergcarlos@gmail.com>
2022-05-28 13:39:51 -0300

    Criando o Enum para o OrderStatus

--- src/main/java/com/carlosguttemberg/course/config/TestConfig.java
+++ src/main/java/com/carlosguttemberg/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.carlosguttemberg.course.config;
 
 import com.carlosguttemberg.course.entities.Order;
 import com.carlosguttemberg.course.entities.User;
+import com.carlosguttemberg.course.entities.enums.OrderStatus;
 import com.carlosguttemberg.course.repositories.OrderRepository;
 import com.carlosguttemberg.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
 
         this.userRepository.saveAll(Arrays.asList(u1, u2));
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
         this.orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     }
 }
