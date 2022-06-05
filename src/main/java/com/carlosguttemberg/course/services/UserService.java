c7ac77d248ec633980f1f922c68893b1fb3fda68
Carlos Guttemberg <guttembergcarlos@gmail.com>
2022-06-05 11:38:41 -0300

    Criando o tratamento de excecao ao remover user

--- src/main/java/com/carlosguttemberg/course/services/UserService.java
+++ src/main/java/com/carlosguttemberg/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.carlosguttemberg.course.services;
 
 import com.carlosguttemberg.course.entities.User;
 import com.carlosguttemberg.course.repositories.UserRepository;
+import com.carlosguttemberg.course.services.exceptions.DatabaseException;
 import com.carlosguttemberg.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,7 +27,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        this.repository.deleteById(id);
+        try {
+            this.repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
