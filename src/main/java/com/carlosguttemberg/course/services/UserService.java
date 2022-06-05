d9bc467e54a0a2aa0b96ae9547d709270272625f
Carlos Guttemberg <guttembergcarlos@gmail.com>
2022-06-05 11:44:58 -0300

    Tratativa de error no update

--- src/main/java/com/carlosguttemberg/course/services/UserService.java
+++ src/main/java/com/carlosguttemberg/course/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,10 +38,15 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getOne(id);
-        updateData(entity, obj);
+        try {
+            User entity = repository.getOne(id);
+            updateData(entity, obj);
 
-        return repository.save(entity);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User obj) {
